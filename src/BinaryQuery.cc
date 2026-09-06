fe8080acf9bc3007cec7f6594994ef7672f485d8
Jun19204 <thomas19204@gmail.com>
2026-09-06 21:44:11 +0900

    대망의 Query문제 수정

--- src/BinaryQuery.cc
+++
@@ -1,9 +0,0 @@
-#include "rQuery/BinaryQuery.h"
-
-#include <string>
-
-std::string BinaryQuery::rep() const {
-    return "(" + lhs_.rep() + 
-           " " + op_sym_ + 
-           " " + rhs_.rep() + ")";
-}
