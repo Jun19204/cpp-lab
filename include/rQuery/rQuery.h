f9a8de9532f0cdfbc5cfee558d0d1fbbee5e2e94
Jun19204 <thomas19204@gmail.com>
2026-09-07 22:51:28 +0900

    공유 자원을 클래스(SharedData)로 분리 및 스마트포인터 없이 Handle클래스(TextQuery)에서 ownership 관리

--- include/rQuery/rQuery.h
+++ include/rQuery/rQuery.h
@@ -10,14 +10,52 @@
 
 class QueryResult;
 
+class SharedData {
+public:
+  using LineNo = std::vector<std::string>::size_type;
+  explicit SharedData(std::ifstream& file);
+  SharedData(const SharedData&) = delete;
+  SharedData(SharedData&&) = delete;
+  SharedData& operator=(const SharedData&) = delete;
+  SharedData& operator=(SharedData&&) = delete;
+  ~SharedData() = default;
+
+  void addRef() {
+    ++use_count_;
+  }
+  bool release() {
+    return --use_count_ == 0;
+  }
+
+private:
+  std::vector<std::string> file_;
+  std::map<std::string, std::set<LineNo>> word_map_;
+  std::size_t use_count_{1};
+};
+
+SharedData::SharedData(std::ifstream& infile) {
+  std::string text;
+  while (std::getline(infile, text)) {
+    file_.push_back(text);
+
+    const auto line_number{file_.size() - 1};
+    std::istringstream line(text);
+
+    std::string word;
+    while (line >> word) {
+      auto& lines = word_map_[word];
+      lines.insert(line_number);
+    }
+  }
+}
+
+
 // file을 읽고 map을 생성하는 클래스
 class TextQuery {
 public:
-  using LineNo = std::vector<std::string>::size_type;
-
-  explicit TextQuery(std::ifstream& file);
+  explicit TextQuery(std::ifstream& infile);
   explicit TextQuery(const TextQuery& text_query);
-  explicit TextQuery(TextQuery&& text_qeury) noexcept;
+  explicit TextQuery(TextQuery&& text_query) noexcept;
   TextQuery& operator=(const TextQuery& rhs) &;
   TextQuery& operator=(TextQuery&& rhs) & noexcept;
   ~TextQuery();
@@ -25,45 +63,58 @@ public:
   [[nodiscard]] QueryResult query(const std::string&) const;
 
 private:
-  std::vector<std::string>* file_{nullptr};
-  std::size_t* use_file_{nullptr};
-  std::map<std::string, std::set<LineNo>*> word_map_;
-  std::size_t* use_set_{nullptr};
-};
-
-TextQuery::TextQuery(std::ifstream& file)
-  : file_(new std::vector<std::string>()), use_file_(new size_t(1))
-{
-  std::string text;
-
-  while (std::getline(file, text)) {
-    file_->push_back(text);
-    
-    const auto line_number = file_->size() - 1;
-    std::istringstream line(text);
-    
-    std::string word;
-    
-    while (line >> word) {
-      auto* lines = word_map_[word];
-      
-      if (lines == nullptr) {
-        lines = new std::set<LineNo>();
-        use_set_ = new size_t(1);
-      }
-      lines->insert(line_number);
+  SharedData* data_;
+  void release() {
+    if (data_ == nullptr) {
+      return;
+    }
+    if (data_->release()) {
+      delete data_;
+      data_ = nullptr;
     }
   }
+  void addRef() {
+    if (data_ != nullptr) {
+      data_->addRef();
+    }
+  }
+};
+
+TextQuery::TextQuery(std::ifstream& infile) 
+  : data_(new SharedData(infile)) {}
+
+TextQuery::TextQuery(const TextQuery& text_query) 
+  : data_(text_query.data_)
+{
+  addRef();
 }
 
-TextQuery::TextQuery(const TextQuery& text_query) {
-  file_ = text_query.file_;
-  ++(*use_file_);
-  --(*text_query.use_file_);
-  word_map_ = text_query.word_map_;
-  ++(*use_set_);
-  --(*text_query.use_set_);
+TextQuery::TextQuery(TextQuery&& text_query) noexcept {
+  data_ = text_query.data_;
+  text_query.data_ = nullptr;
 }
 
+TextQuery& TextQuery::operator=(const TextQuery& rhs) & {
+  if (this == &rhs) {
+    return *this;
+  }
+  release();
+  data_ = rhs.data_;
+  addRef();
+  return *this;
+}
 
+TextQuery& TextQuery::operator=(TextQuery&& rhs) & noexcept {
+  if (this == &rhs) {
+    return *this;
+  }
+  release();
+  data_ = rhs.data_;
+  rhs.data_ = nullptr;
+  return *this;
+}
+
+TextQuery::~TextQuery() {
+  release();
+}
 
