90b2f58139d9b5c9b83c632dc1e6d802371d3dc8
Jun19204 <thomas19204@gmail.com>
2026-09-13 23:22:58 +0900

    feat: StrBlob --> Blob 템플릿 클래스로 업그레이드

--- include/Blob/Blob.h
+++ include/Blob/Blob.h
@@ -2,6 +2,7 @@
 
 #include <initializer_list>
 #include <memory>
+#include <stdexcept>
 #include <vector>
 #include <utility>
 
@@ -9,23 +10,63 @@ template <typename T> class Blob {
 public:
   using value_type = T;
   using size_type = typename std::vector<T>::size_type;
-  // Constructor
+  // constructors
   Blob();
   Blob(std::initializer_list<T> il);
-  // Counts of element in Blob
+  // number of element in Blob
   size_type size() const { return data_->size(); }
   [[nodiscard]]
   bool empty() const { return data_->empty(); }
-  // Insert and delete element
+  // add and remove element
   void push_back(const T& t) { data_->push_back(t); }
   void push_back(T&& t) { data_->push_back(std::move(t)); }
   void pop_back() { data_->pop_back(); }
-  // Access to element
+  // access to element
+  T& front();
+  const T& front() const;
   T& back();
   const T& back() const;
-  T& operator[] (size_type i);
-  const T& operator[] (size_type i) const;
+  T& operator[] (size_type i) { return data_[i]; }
+  const T& operator[] (size_type i) const { return data_[i]; };
 private:
   std::shared_ptr<std::vector<T>> data_;
+  // throws msg if data[i] isn't valid
   void check(size_type i, const std::string& msg) const;
 };
+
+template <typename T>
+Blob<T>::Blob() : data_(std::make_shared<std::vector<T>>()) {}
+
+template <typename T>
+Blob<T>::Blob(std::initializer_list<T> il) 
+: data_(std::make_shared<std::vector<T>>(il)) {}
+
+template <typename T>
+void Blob<T>::check(size_type i, const std::string& msg) const {
+  if (i >= data_->size()) {
+    throw std::out_of_range(msg);
+  }
+}
+
+template <typename T>
+T& Blob<T>::front() {
+  check(0, "front on empty Blob");
+  return data_->front();
+}
+template <typename T>
+const T& Blob<T>::front() const {
+  check(0, "front on empty Blob");
+  return data_->front();
+}
+
+template <typename T>
+T& Blob<T>::back() {
+  check(0, "back on empty Blob");
+  return data_->back();
+}
+template <typename T>
+const T& Blob<T>::back() const {
+  check(0, "back on empty Blob");
+  return data_->back();
+}
+
